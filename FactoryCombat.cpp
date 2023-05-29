bac0eee54d5e971bf3c8ed243654b5f6827ab0f4
Lon van Ettinger <lonvanettinger@gmail.com>
2023-05-29 16:38:41 +0200

    changed variable names with g prefix to without to avoid confusion

--- FactoryCombat.cpp
+++ FactoryCombat.cpp
@@ -31,11 +31,11 @@ int main()
 	srand((unsigned)time(NULL));
 
 	// initialize game variables
-	EnemyFactory* gpEnemyFactory = new EnemyFactory;
-	Player* gpPlayer = new Player();
-	Actor* gpCurrentEnemy = gpEnemyFactory->CreateEnemy(GetRandomNum(0, 3));
-	string gInput = " ";
-	bool gInputIsNotValid = true;
+	EnemyFactory* pEnemyFactory = new EnemyFactory;
+	Player* pPlayer = new Player();
+	Actor* pCurrentEnemy = pEnemyFactory->CreateEnemy(GetRandomNum(0, 3));
+	string input = " ";
+	bool inputIsNotValid = true;
 
 	// start game text
 	cout << "Welcome to FactoryCombat! This is a small,\n";
@@ -43,20 +43,20 @@ int main()
 	cin.get();
 
 	// get player name
-	string gPlayerName;
+	string playerName;
 	cout << "Dear traveller, what is your name? ";
-	cin >> gPlayerName;
-	cout << "\nWonderful! Let us begin, " + gPlayerName + "!\n\n";
+	cin >> playerName;
+	cout << "\nWonderful! Let us begin, " + playerName + "!\n\n";
 	cin.get();
 
 	// first enemy text
 	cout << "An enemy approaches...";
 	cin.get();
-	cout << "It's a " + gpCurrentEnemy->GetName() + "!";
+	cout << "It's a " + pCurrentEnemy->GetName() + "!";
 	cin.get();
 
 	// main game loop
-	while (gpPlayer->mIsAlive)
+	while (pPlayer->mIsAlive)
 	{
 		// player actions options text
 		cout << "It looks hostile.. What will you do?\n";
@@ -65,45 +65,45 @@ int main()
 		cout << "3 Do nothing\n";
 
 		// player action choice texts and executions
-		gInputIsNotValid = true;
-		while (gInputIsNotValid)
+		inputIsNotValid = true;
+		while (inputIsNotValid)
 		{
-			cin >> gInput;
+			cin >> input;
 			// ATTACK
-			if (gInput == "1")
+			if (input == "1")
 			{
-				gInputIsNotValid = false;
+				inputIsNotValid = false;
 
 				// player attacks
-				gpCurrentEnemy->TakeDamage(gpPlayer->Attack());
-				cout << gPlayerName + " did " + to_string(gpPlayer->Attack())
-					+ " damage to " + gpCurrentEnemy->GetName() + "!\n";
+				pCurrentEnemy->TakeDamage(pPlayer->Attack());
+				cout << playerName + " did " + to_string(pPlayer->Attack())
+					+ " damage to " + pCurrentEnemy->GetName() + "!\n";
 
 				// check if enemy died
-				if (!gpCurrentEnemy->mIsAlive)
+				if (!pCurrentEnemy->mIsAlive)
 				{
-					cout << gpCurrentEnemy->GetName() + " has been defeated!\n";
+					cout << pCurrentEnemy->GetName() + " has been defeated!\n";
 				}
 				else
 				{
 					// enemy attacks
-					gpPlayer->TakeDamage(gpCurrentEnemy->Attack());
-					cout << gpCurrentEnemy->GetName() + " did "
-						+ to_string(gpCurrentEnemy->Attack()) + " damage to "
-						+ gPlayerName + "!\n";
+					pPlayer->TakeDamage(pCurrentEnemy->Attack());
+					cout << pCurrentEnemy->GetName() + " did "
+						+ to_string(pCurrentEnemy->Attack()) + " damage to "
+						+ playerName + "!\n";
 					// player status
-					cout << gPlayerName + " has " + to_string(gpPlayer->GetHealth())
+					cout << playerName + " has " + to_string(pPlayer->GetHealth())
 						+ "HP left!\n";
 				}
 				cin.get();
 			}
 			// RUN AWAY
-			else if (gInput == "2") 
+			else if (input == "2") 
 			{
-				gInputIsNotValid = false;
+				inputIsNotValid = false;
 
 				// (try to) run away
-				cout << gPlayerName + " tries to run away.. ";
+				cout << playerName + " tries to run away.. ";
 				cin.get();
 				// 2 : 1 chance of succeeding/not succeeding to run away
 				switch (GetRandomNum(0, 2))
@@ -111,93 +111,93 @@ int main()
 				case 0:
 				case 1: // runs away
 					cout << "their attempt was succesful!";
-					gpCurrentEnemy->mIsAlive = false;
+					pCurrentEnemy->mIsAlive = false;
 					break;
 
 				case 2: // can't escape
 					cout << "their attempt was not succesful!\n";
 					// enemy attacks
-					gpPlayer->TakeDamage(gpCurrentEnemy->Attack());
-					cout << gpCurrentEnemy->GetName() + " did "
-						+ to_string(gpCurrentEnemy->Attack()) + " damage to "
-						+ gPlayerName + "!\n";
+					pPlayer->TakeDamage(pCurrentEnemy->Attack());
+					cout << pCurrentEnemy->GetName() + " did "
+						+ to_string(pCurrentEnemy->Attack()) + " damage to "
+						+ playerName + "!\n";
 					// player status
-					cout << gPlayerName + " has " + to_string(gpPlayer->GetHealth())
+					cout << playerName + " has " + to_string(pPlayer->GetHealth())
 						+ "HP left!\n";
 				}
 				cin.get();
 			}
 			// DO NOTHING
-			else if (gInput == "3")
+			else if (input == "3")
 			{
-				gInputIsNotValid = false;
+				inputIsNotValid = false;
 
 				// enemy attacks
-				gpPlayer->TakeDamage(gpCurrentEnemy->Attack());
-				cout << gpCurrentEnemy->GetName() + " did "
-					+ to_string(gpCurrentEnemy->Attack()) + " damage to "
-					+ gPlayerName + "!";
+				pPlayer->TakeDamage(pCurrentEnemy->Attack());
+				cout << pCurrentEnemy->GetName() + " did "
+					+ to_string(pCurrentEnemy->Attack()) + " damage to "
+					+ playerName + "!";
 				cin.get();
 				// player status
-				cout << gPlayerName + " has " + to_string(gpPlayer->GetHealth())
+				cout << playerName + " has " + to_string(pPlayer->GetHealth())
 					+ "HP left!\n";
 				cin.get();
 			}
 		}
 
 		// spawn new enemy if it died
-		if (!gpCurrentEnemy->mIsAlive)
+		if (!pCurrentEnemy->mIsAlive)
 		{
 			// cleanup and spawn new enemy
-			delete gpCurrentEnemy;
-			gpCurrentEnemy = gpEnemyFactory->CreateEnemy(GetRandomNum(0, 3));
+			delete pCurrentEnemy;
+			pCurrentEnemy = pEnemyFactory->CreateEnemy(GetRandomNum(0, 3));
 
 			// respawn text
 			cout << "A new enemy approaches...";
 			cin.get();
-			cout << "It's a " + gpCurrentEnemy->GetName() + "!";
+			cout << "It's a " + pCurrentEnemy->GetName() + "!";
 		}
 
 
 		cin.get();
 		// if player dies
-		if (!gpPlayer->mIsAlive)
+		if (!pPlayer->mIsAlive)
 		{
-			cout << "Oh god! You look beaten up, " + gPlayerName + "...\n";
+			cout << "Oh god! You look beaten up, " + playerName + "...\n";
 			// give player a chance to start over
 			cout << "Would you like to try again? Y/N ";
-			while (gInputIsNotValid)
+			while (inputIsNotValid)
 			{
-				cin >> gInput;
+				cin >> input;
 				// check if input is valid
-				if (gInput == "Y" && gInput == "y")
+				if (input == "Y" && input == "y")
 				{
-					gInputIsNotValid = false;
+					inputIsNotValid = false;
 
 					// reset player
 					cout << "\nGreat! Here we go again..\n\n";
-					gpPlayer->Reset();
+					pPlayer->Reset();
 
 					// cleanup current and spawn new enemy
-					delete gpCurrentEnemy;
-					gpCurrentEnemy = gpEnemyFactory->CreateEnemy(GetRandomNum(0, 3));
+					delete pCurrentEnemy;
+					pCurrentEnemy = pEnemyFactory->CreateEnemy(GetRandomNum(0, 3));
 					// first enemy text
 					cout << "An enemy approaches...";
 					cin.get();
-					cout << "It's a " + gpCurrentEnemy->GetName() + "!";
+					cout << "It's a " + pCurrentEnemy->GetName() + "!";
 					cin.get();
 				}
-				else if (gInput == "N" && gInput == "n")
+				else if (input == "N" && input == "n")
 				{
-					gInputIsNotValid = false;
+					inputIsNotValid = false;
 				}
 			}
 		}
 	}
 
 	// cleanup and end game text
-	delete gpPlayer;
-	cout << "\n\n\nThank you for playing " + gPlayerName + "!";
+	delete pPlayer;
+	cout << "\n\n\nThank you for playing " + playerName + "!";
 
 	cin.get();
 
